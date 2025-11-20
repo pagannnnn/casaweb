a6f82b59aafe14de91a1e9ce3a6f221b29a11520
Diego <diegoserr091111@gmail.com>
2025-11-19 23:05:59 -0600

    actualizacion

--- HTML/administrador /provedores.php
+++ HTML/administrador /provedores.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Proveedores</title>
-  <link rel="stylesheet" href="../css/proveedor.css">
+  <link rel="stylesheet" href="../../css/proveedor.css">
 </head>
 
 <body>
